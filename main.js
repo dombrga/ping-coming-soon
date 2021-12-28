5edf10a45189500d15dd979d5eff026ca7ed2121
dobrga <dom.barga@gmail.com>
2021-12-28 17:28:07 +0800

    Changed some divs to section. Updated README.

--- main.js
+++ main.js
@@ -7,8 +7,7 @@ function checkEmail(e) {
   e.preventDefault()
   const email = document.querySelector('#email')
   const emailAdd = email.value
-  console.log(emailAdd)
-  var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   
   // if empty
